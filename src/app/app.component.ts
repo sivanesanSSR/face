6cc26403d0e791b56cbaa57939f1b225c563b4ce
sivanesan <sivanesantrt@gmail.com>
2024-05-15 21:37:11 +0530

    file handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,133 +1,209 @@
-import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import * as faceapi from 'face-api.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'webrtc';
-  // if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  //   // Not adding `{ audio: true }` since we only want video now
-  //   navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-  //     //video.src = window.URL.createObjectURL(stream);
-  //     video.srcObject = stream;
-  //     video.play();
-  //   });
+  ctx;
+  imageCapture: any = null;
+  @ViewChild('video') private video: ElementRef;
+  @ViewChild('canvas') private canvas: ElementRef;
+ 
+  constructor(private zone: NgZone,private elRef: ElementRef) {
+    // this.zone.runOutsideAngular(() => {
+    //   if (navigator.mediaDevices) {
+    //     console.log("true to display media")
+    //   }
+    //   navigator.mediaDevices.getUserMedia({ video: true })
+    //     .then((stream: any) => {
 
-  // }
+    //       // this is outside a zone
+    //       // const myBlob = new Blob(stream);
+    //       this.zone.run(() => {
+    //         this.video.nativeElement.srcObject = stream;
 
-  @ViewChild('video') private video!:ElementRef;
-  @ViewChild('canvas') private canvas!:ElementRef;
-  ctx:any
-    constructor(private zone:NgZone)
-  {  
-    
-  this.zone.runOutsideAngular(() => {
-      if(navigator.mediaDevices )
-        {
-          console.log("true to display media")
-        }
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream:any) => {
-
-        // this is outside a zone
-        // const myBlob = new Blob(stream);
-        this.zone.run(() => {
-          this.video.nativeElement.srcObject = stream;
-
-          // Create URL for the MediaStream object
-          // const streamUrl = URL.createObjectURL(stream);
-          // console.log(streamUrl); // This should output the URL
-           // and again inside a zone
-          //  this.video.nativeElement.src =stream;
-           this.video.nativeElement.load();
-           this.video.nativeElement.play();
-        });
-      })
-      .catch(err => console.error(err));
-
-
-      
-  });
-
-    // this.video
+    //         // Create URL for the MediaStream object
+    //         // const streamUrl = URL.createObjectURL(stream);
+    //         // console.log(streamUrl); // This should output the URL
+    //         // and again inside a zone
+    //         //  this.video.nativeElement.src =stream;
+    //         this.video.nativeElement.load();
+    //         this.video.nativeElement.play();
+    //       });
+    //     })
+    //     .catch(err => console.error(err));
+    // });
   }
 
-  vidoeEnable()
-  {
+  ngOnInit() {
+    // console.log(this.canvas.nativeElement)
+    // this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.loadModels();
+  }
+
+  vidoeEnable() {
     this.zone.runOutsideAngular(() => {
-      if(navigator.mediaDevices )
-        {
-          console.log("true to display media")
-        }
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream:any) => {
+      if (navigator.mediaDevices) {
+        console.log("true to display media")
+      }
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream: any) => {
+          this.zone.run(() => {
+            this.video.nativeElement.srcObject = stream;
+            this.video.nativeElement.load();
+            this.video.nativeElement.play();
+          });
+        })
+        .catch(err => console.error(err));
+    });
 
-        // this is outside a zone
-        // const myBlob = new Blob(stream);
-        this.zone.run(() => {
-          this.video.nativeElement.srcObject = stream;
-
-          // Create URL for the MediaStream object
-          // const streamUrl = URL.createObjectURL(stream);
-          // console.log(streamUrl); // This should output the URL
-           // and again inside a zone
-          //  this.video.nativeElement.src =stream;
-           this.video.nativeElement.load();
-           this.video.nativeElement.play();
-        });
-      })
-      .catch(err => console.error(err));
-  });
-
-  navigator.mediaDevices.getUserMedia({ video: false, audio: true })
-  .then((stream) => {
-      //  this.localStream = stream;
-       // Do something with the stream
-     })
-  .catch((error) => {
-       console.error('Error accessing media devices.', error);
-     });
+    // navigator.mediaDevices.getUserMedia({ video: false, audio: false })
+    //   .then((stream) => {
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error accessing media devices.', error);
+    //   });
   }
 
-  vidoeDisable()
-  {
+  vidoeDisable() {
     if (this.video.nativeElement.srcObject) {
       this.video.nativeElement.srcObject.getVideoTracks()[0].stop();
       this.video.nativeElement.srcObject = null;
     }
   }
 
-  ngOnInit()
-  {
-    console.log(this.canvas.nativeElement)
-    this.ctx = this.canvas.nativeElement.getContext('2d');
-  }
-  imageCapture:any = null;
-  takeSnapshot()
-  {
-    
-   
-    console.log(this.ctx)
-    this.ctx.drawImage(this.video, 0,0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  takeSnapshot() {
+    this.ctx.drawImage(this.video, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
-  // requestPermission()
-  // { this.zone.runOutsideAngular(() => {
+
+  async loadModels() {
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+  }
   
-  //     navigator.mediaDevices
-  //       .getUserMedia({ video: false, audio: true })
-  //       .then((stream) => {
-  //         this.localStream = stream // A
-  //         // window.localAudio.srcObject = stream; // B
-  //         // window.localAudio.autoplay = true; // C
-  //       })
-  //       .catch((err) => {
-  //         console.error(`you got an error: ${err}`);
-  //       });
-  // });
- 
+  // Detect faces in an image
+  async detectFaces(imageElement) {
+    const detections = await faceapi.detectAllFaces(imageElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+    return detections;
+  }
+  
+  // Example usage
+  async processImage(imageElement) {
+    await this.loadModels();
+    const detections = await this.detectFaces(imageElement);
+    // Process detections as needed
+  }
+  // captureImage() {
+  //   this.imageCapture = this.canvas.nativeElement.toDataURL('image/png');
+  //   this.zone.run(() => {
+  //     this.image.nativeElement.src = this.imageCapture;
+  //   });
   // }
-  // localStream!: MediaStream;
+
+  // ====== > tensorflow <======
+  getevents(){
+    console.log(faceapi);
+  }
+
+  mtcnnForwardParams:any = {
+    // number of scaled versions of the input image passed through the CNN
+    // of the first stage, lower numbers will result in lower inference time,
+    // but will also be less accurate
+    maxNumScales: 10,
+    // scale factor used to calculate the scale steps of the image
+    // pyramid used in stage 1
+    scaleFactor: 0.709,
+    // the score threshold values used to filter the bounding
+    // boxes of stage 1, 2 and 3
+    scoreThresholds: [0.6, 0.7, 0.7],
+    // mininum face size to expect, the higher the faster processing will be,
+    // but smaller faces won't be detected
+    minFaceSize: 200
+  }
+
+  mtcnnResults:any;
+  async  ngAfterViewInit() {
+    this.mtcnnResults = await faceapi.mtcnn(this.video.nativeElement, this.mtcnnForwardParams)
+    let data= { withScore: false }
+    faceapi.draw.drawDetections('overlay', this.mtcnnResults.map(res => res.faceDetection))
+    faceapi.draw.drawFaceLandmarks('overlay', this.mtcnnResults.map(res => res.faceLandmarks))
+
+  }
+
+//   WIDTH = 440;
+// HEIGHT = 280;
+// @ViewChild('video',{ static: true })
+// public video: ElementRef;
+// @ViewChild('canvas',{ static: true })
+// public canvasRef: ElementRef;
+// stream: any;
+// detection: any;
+// resizedDetections: any;
+// canvas: any;
+// canvasEl: any;
+// displaySize: any;
+// videoInput: any;
+
+
+// async ngOnInit() {
+//   await Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri('../../assets/models'),
+//   await faceapi.nets.faceLandmark68Net.loadFromUri('../../assets/models'),
+//   await faceapi.nets.faceRecognitionNet.loadFromUri('../../assets/models'),
+//   await faceapi.nets.faceExpressionNet.loadFromUri('../../assets/models'),]).then(() => this.startVideo());
+//   }
+
+//   startVideo() {
+//     this.videoInput = this.video.nativeElement;
+//     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+//       .then((stream) => {
+//         this.videoInput.srcObject = stream
+//         this.videoInput.load();
+//         this.videoInput.play();
+//         // this.detect_Faces();
+//       })
+//       .catch((error) => {
+//         console.error('Error accessing media devices.', error);
+//       });
+ 
+//     }
+//     onMediaLoad()
+//     {
+//       this.detect_Faces();
+//     }
+//     mediaLoaded:boolean=false;
+//     async detect_Faces() {
+//       this.elRef.nativeElement.querySelector('video').addEventListener('play', async () => {
+//        this.canvas = await faceapi.createCanvasFromMedia(this.videoInput);
+//        this.canvasEl = this.canvasRef.nativeElement;
+//        this.canvasEl.appendChild(this.canvas);
+//        this.canvas.setAttribute('id', 'canvass');
+//        this.canvas.setAttribute(
+//           'style',`position: fixed;
+//           top: 0;
+//           left: 0;`
+//        );
+//        this.displaySize = {
+//           width: this.videoInput.width,
+//           height: this.videoInput.height,
+//        };
+//        faceapi.matchDimensions(this.canvas, this.displaySize);
+//        setInterval(async () => {
+//          this.detection = await faceapi.detectAllFaces(this.videoInput,  new  faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+//          this.mediaLoaded = true;
+//          this.resizedDetections = faceapi.resizeResults(
+//             this.detection,
+//             this.displaySize
+//           );
+//          this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width,this.canvas.height);
+//          faceapi.draw.drawDetections(this.canvas, this.resizedDetections);
+//          faceapi.draw.drawFaceLandmarks(this.canvas, this.resizedDetections);
+//          faceapi.draw.drawFaceExpressions(this.canvas, this.resizedDetections);
+//       }, 100);
+//       });
+//       }
 }
